b451e8804a32d53fa23788cad40f1ffb107bf786
Hai Ng <haibeo@gmail.com>
2016-07-15 16:11:16 +0700

    update turn

--- main.js
+++ main.js
@@ -27,13 +27,27 @@ import {
 
 const socket = io.connect('youcall.io', {transports: ['websocket']});
 const configuration = {"iceServers": [
-      {url:"stun:188.166.191.174:3478"},
-  {url:"turn:188.166.191.174:3478"},
-{url:'stun:stun.l.google.com:19302'},
-  {url:'stun:stun1.l.google.com:19302'},
-  {url:'stun:stun2.l.google.com:19302'},
-  {url:'stun:stun3.l.google.com:19302'},
-  {url:'stun:stun4.l.google.com:19302'},
+    {url:"stun:188.166.191.174:3478"},
+    {url:'stun:stun.l.google.com:19302'},
+    {url:'stun:stun1.l.google.com:19302'},
+    {url:'stun:stun2.l.google.com:19302'},
+    {url:'stun:stun3.l.google.com:19302'},
+    {url:'stun:stun4.l.google.com:19302'},
+    {
+	url: 'turn:numb.viagenie.ca',
+	credential: 'muazkh',
+	username: 'webrtc@live.com'
+},
+{
+	url: 'turn:192.158.29.39:3478?transport=udp',
+	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	username: '28224511:1379330808'
+},
+{
+	url: 'turn:192.158.29.39:3478?transport=tcp',
+	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	username: '28224511:1379330808'
+}
 
 ]};
 const pcPeers = {};
