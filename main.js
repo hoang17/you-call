4ea5fe818a5d6b3f5ecc16c3a57bd3a3d72ad616
Hai Ng <haibeo@gmail.com>
2016-07-15 16:33:01 +0700

    update numb turn

--- main.js
+++ main.js
@@ -27,27 +27,20 @@ import {
 
 const socket = io.connect('youcall.io', {transports: ['websocket']});
 const configuration = {"iceServers": [
+
+    {
+	url: 'turn:numb.viagenie.ca',
+	credential: 'youcal123',
+	username: 'jinnguyen019@gmail.com'
+},
     {url:"stun:188.166.191.174:3478"},
     {url:'stun:stun.l.google.com:19302'},
     {url:'stun:stun1.l.google.com:19302'},
     {url:'stun:stun2.l.google.com:19302'},
     {url:'stun:stun3.l.google.com:19302'},
     {url:'stun:stun4.l.google.com:19302'},
-    {
-	url: 'turn:numb.viagenie.ca',
-	credential: 'muazkh',
-	username: 'webrtc@live.com'
-},
-{
-	url: 'turn:192.158.29.39:3478?transport=udp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
-},
-{
-	url: 'turn:192.158.29.39:3478?transport=tcp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
-}
+    
+
 
 ]};
 const pcPeers = {};
