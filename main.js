3385dfe9d31d760b7883045e28eeb71529225e0a
Hai Ng <haibeo@gmail.com>
2016-07-18 00:04:58 +0700

    ice servers

--- main.js
+++ main.js
@@ -34,18 +34,18 @@ const configuration = {"iceServers": [
     	username: 'client'
     },
 
-//     {
-// 	url: 'turn:numb.viagenie.ca',
-// 	credential: 'youcal123',
-// 	username: 'jinnguyen019@gmail.com'
-// },
-//     {url:"stun:188.166.191.174:3478"},
-//     {url:'stun:stun.l.google.com:19302'},
-//     {url:'stun:stun1.l.google.com:19302'},
-//     {url:'stun:stun2.l.google.com:19302'},
-//     {url:'stun:stun3.l.google.com:19302'},
-//     {url:'stun:stun4.l.google.com:19302'},
-//
+    {
+	url: 'turn:numb.viagenie.ca',
+	credential: 'youcal123',
+	username: 'jinnguyen019@gmail.com'
+},
+    {url:"stun:188.166.191.174:3478"},
+    {url:'stun:stun.l.google.com:19302'},
+    {url:'stun:stun1.l.google.com:19302'},
+    {url:'stun:stun2.l.google.com:19302'},
+    {url:'stun:stun3.l.google.com:19302'},
+    {url:'stun:stun4.l.google.com:19302'},
+
 
 
 ]};
